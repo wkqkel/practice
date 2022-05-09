75f2d464bc6c6f06a060906e9ee88ceb479b1eee
Sangwon Park <wkqkel@naver.com>
2022-05-09 18:25:21 +0900

    Update index.ts

--- quiz/2_address-book/src/index.ts
+++ quiz/2_address-book/src/index.ts
@@ -1,10 +1,4 @@
-// 11-1 프로젝트소개
-// 해당폴더 마우스 오른쪽 누르고 open in Intergrated terminal 들어와서 yarn install
-// 11-2 프로젝트 실습 방법안내
-// tsConfigJson에 noImplicitAny,strict, strictFunctionTypes을 true로 바꿔주면 로직파일에 에러가 남
-// .eslintc.js가서 또 typescript off돼있는걸 주석처리하면 api쪽 펑션보면 에러 남.
 
-// phones안의 phone은 그냥 변수로 아래보면 키값인 phone은 home이될수도 office가 될수도 있음
 interface PhoneNumberDictionary {
   [phone: string]: {
     num: number;
@@ -26,11 +20,6 @@ enum PhoneType {
 }
 
 // api
-// TODO: 아래 함수의 반환 타입을 지정해보세요.
-// fetchContacts는 api묘사함수로 리턴값이 프로미스
-// api호출해와서 응답의 규격을 정의할 때 제네릭을 가장 많이 사용
-
-// Contact라는 인터페이스의 배열을 Promise의 반환값으로 받겠다
 function fetchContacts(): Promise<Contact[]> {
   // TODO: 아래 변수의 타입을 지정해보세요.
 
@@ -44,27 +33,6 @@ function fetchContacts(): Promise<Contact[]> {
         },
         office: {
           num: 44455556666,
-        },
-      },
-    },
-    {
-      name: "Banner",
-      address: "New York",
-      phones: {
-        home: {
-          num: 77788889999,
-        },
-      },
-    },
-    {
-      name: "마동석",
-      address: "서울시 강남구",
-      phones: {
-        home: {
-          num: 213423452,
-        },
-        studio: {
-          num: 314882045,
         },
       },
     },
