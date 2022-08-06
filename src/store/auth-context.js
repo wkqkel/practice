8528f494d25b4b6880caa8e6670cc63cad89ffa9
sangwon <swpark@trineedle.com>
2022-08-06 23:12:47 +0900

    로그인유지_로컬스토리지

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -8,15 +8,19 @@ const AuthContext = React.createContext({
 });
 
 export const AuthContextProvider = (props) => {
-  const [token, setToken] = useState(null);
+  const initialToken = localStorage.getItem("token");
+  const [token, setToken] = useState(initialToken);
   const userIsLoggedIn = !!token;
 
   const loginHandler = (token) => {
     setToken(token);
+    // 로컬스토리지에는 기본형(문자열,숫자)만 저장 가능, jwt토큰은 그대로. 만약 객체형식으로 넣고싶다면 json화시켜줘야함
+    localStorage.setItem("token", token);
   };
 
   const logoutHandler = () => {
     setToken(null);
+    localStorage.removeItem("token");
   };
 
   const contextValue = {
