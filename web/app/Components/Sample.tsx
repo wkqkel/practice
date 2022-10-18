83a43294ff85eb5a8bd8cec5da7b1fe7b106478b
sangwon <wkqkel@naver.com>
2022-10-19 02:38:48 +0900

    initialize: remix project

---
+++ web/app/Components/Sample.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Sample = () => {
+  return <Block>Block</Block>;
+};
+
+const Block = styled.div`
+  background: blue;
+`;
+
+export default Sample;
---
+++ web/app/root.tsx
@@ -0,0 +1,28 @@
+import type { MetaFunction } from "@remix-run/node";
+import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react";
+import GlobalStyle from "./Components/GlobalStyle";
+
+export const meta: MetaFunction = () => ({
+  charset: "utf-8",
+  title: "New Remix App",
+  viewport: "width=device-width,initial-scale=1",
+});
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <Links />
+        {typeof document === "undefined" ? "__STYLES__" : null}
+      </head>
+      <body>
+        <GlobalStyle />
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
