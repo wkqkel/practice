ff6728944e803a64670d9ed369019f1b66a21ad0
sangwon <sangwon@sangwonui-MacBookPro.local>
2023-07-30 14:46:36 +0900

    FEAT: 회원가입페이지 구현

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -8,6 +8,7 @@ const ctrl = require("./home.ctrl");
 // ctrl.home 보다는 이후 ctrl.output.home으로 해서 홈을 출력하구나를 좀 더 명시적으로.
 router.get("/", ctrl.output.home);
 router.get("/login", ctrl.output.login);
+router.get("/register", ctrl.output.register);
 router.post("/login", ctrl.process.login);
 
 // app.js와 연결해줄 필요가 있음
