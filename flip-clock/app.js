dc7311fa013c70f701c202cf2eaf75438eaf1545
sangwon <sangwon@sangwonui-MacBookPro.local>
2023-10-02 02:22:48 +0900

    flip-clock

---
+++ flip-clock/app.js
@@ -0,0 +1,121 @@
+const targetDate = "2023-09-22T19:00:00";
+
+function getTimeSegmentElements(segmentElement) {
+  const segmentDisplay = segmentElement.querySelector(".segment-display");
+  const segmentDisplayTop = segmentDisplay.querySelector(
+    ".segment-display__top"
+  );
+  const segmentDisplayBottom = segmentDisplay.querySelector(
+    ".segment-display__bottom"
+  );
+
+  const segmentOverlay = segmentDisplay.querySelector(".segment-overlay");
+  const segmentOverlayTop = segmentOverlay.querySelector(
+    ".segment-overlay__top"
+  );
+  const segmentOverlayBottom = segmentOverlay.querySelector(
+    ".segment-overlay__bottom"
+  );
+  return {
+    segmentDisplayTop,
+    segmentDisplayBottom,
+    segmentOverlay,
+    segmentOverlayTop,
+    segmentOverlayBottom,
+  };
+}
+
+function updateSegmentValues(displayElement, overlayElement, value) {
+  displayElement.textContent = value;
+  overlayElement.textContent = value;
+}
+
+function updateTimeSegment(segmentElement, timeValue) {
+  const segmentElements = getTimeSegmentElements(segmentElement);
+
+  if (
+    parseInt(segmentElements.segmentDisplayTop.textContent, 10) === timeValue
+  ) {
+    return;
+  }
+
+  segmentElements.segmentOverlay.classList.add("flip");
+
+  updateSegmentValues(
+    segmentElements.segmentDisplayTop,
+    segmentElements.segmentOverlayBottom,
+    timeValue
+  );
+
+  function finishAnimation() {
+    segmentElements.segmentOverlay.classList.remove("flip");
+    updateSegmentValues(
+      segmentElements.segmentDisplayBottom,
+      segmentElements.segmentOverlayTop,
+      timeValue
+    );
+
+    this.removeEventListener("animationend", finishAnimation);
+  }
+
+  segmentElements.segmentOverlay.addEventListener(
+    "animationend",
+    finishAnimation
+  );
+}
+
+function updateTimeSection(sectionID, timeValue) {
+  const firstNumber = Math.floor(timeValue / 10);
+  const secondNumber = timeValue % 10;
+
+  const sectionElement = document.getElementById(sectionID);
+  const timeSegments = sectionElement.querySelectorAll(".time-segment");
+
+  updateTimeSegment(timeSegments[0], firstNumber);
+  updateTimeSegment(timeSegments[1], secondNumber);
+}
+
+function getTimeRemaining(targetDateTime) {
+  const nowTime = Date.now();
+  const secondsRemaining = Math.floor((targetDateTime - nowTime) / 1000);
+
+  const complete = nowTime >= targetDateTime;
+
+  if (complete) {
+    return {
+      complete,
+      seconds: 0,
+      minutes: 0,
+      hours: 0,
+    };
+  }
+
+  const hours = Math.floor(secondsRemaining / 60 / 60);
+  const minutes = Math.floor(secondsRemaining / 60) - hours * 60;
+  const seconds = secondsRemaining % 60;
+
+  return {
+    seconds,
+    minutes,
+    hours,
+  };
+}
+
+function updateAllSegments() {
+  const targetTimeStamp = new Date(targetDate).getTime();
+  const timeRemainingBits = getTimeRemaining(targetTimeStamp);
+
+  updateTimeSection("hours", timeRemainingBits.hours);
+  updateTimeSection("minutes", timeRemainingBits.minutes);
+  updateTimeSection("seconds", timeRemainingBits.seconds);
+
+  return timeRemainingBits.complete;
+}
+
+const countdownTimer = setInterval(() => {
+  const isComplete = updateAllSegments();
+
+  if (isComplete) {
+    clearInterval(countdownTimer);
+  }
+}, 1000);
